e86626266bde06f9177f5b7b9bdf00a232ad053b
Magic-Mayo <mike.mayoii@gmail.com>
2019-09-20 00:50:27 -0700

    removed line for html routes

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require('./routes/htmlRoutes')(app);
-// require('./routes/apiRoutes')(app);
+require('./routes/apiRoutes')(app);
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
