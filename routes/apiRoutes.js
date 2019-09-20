d187f22096b8085abd0e2670e74e13730029b0fd
Magic-Mayo <mike.mayoii@gmail.com>
2019-09-20 00:50:27 -0700

    made route for /articles to scrape AP top news and get title, summary, link, authors, and date

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,47 +1,36 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const moment = require('moment');
 
-// module.exports = (app) => {
-//     app.get('/articles', (req,res)=>{
-//         axios.get('https://www.apnews.com/apf-topnews').then(news=>{
-//             const $ = cheerio.load(news.data);
-//             const articles = [];
-
-//             $('div.FeedCard').each((i,elem)=>{
-//                 const title = elem.firstChild().text();
-//                 const summary = elem.children('a.');
-//                 const link = elem.firstChild().href();
-//                 const img = elem.children('img').href();
-
-//                 articles.push({
-//                     title: title,
-//                     summary: summary,
-//                     link: link,
-//                     picture: img
-//                 })
-//             })
-//         })
-//     })
-// }
-
-axios.get('https://www.apnews.com/apf-topnews').then(news=>{
-    const $ = cheerio.load(news.data);
-    const articles = [];
-    console.log($('div.FeedCard').children('a').children('.LazyImage').attr())
-    $('div.FeedCard').each((i,elem)=>{
-        const title = $(elem).children('.CardHeadline').children('.headline').text();
-        const summary = $(elem).children('a').children('.content').text();
-        const link = $(elem).children('.CardHeadline').children('.headline').attr('href');
-        const img = $(elem).children('a').children('.LazyImage').find('img').attr('data-cfsrc');
-
-        articles.push({
-            title: title,
-            summary: summary,
-            link: link,
-            picture: img
+module.exports = (app) => {
+    app.get('/articles', (req, res)=>{
+        axios.get('https://www.apnews.com/apf-topnews').then(news=>{
+            const $ = cheerio.load(news.data);
+            const articles = [];
+            $('div.FeedCard').each((i,elem)=>{
+                const title = $(elem).children('.CardHeadline').children('.headline').text() || $(elem).children('a').children('.CardHeadline').children('.headline').text();
+                const summary = $(elem).children('a').children('.content').text() || 'No summary, please follow link';
+                const link = `https://apnews.com/apf-topnews${$(elem).children('a').attr('href')}`;
+                const author = () => {
+                    if ($(elem).children('.CardHeadline').children('.signature').children('.byline').text() !== ''){
+                        return $(elem).children('.CardHeadline').children('.signature').children('.byline').text().split('By ')[1];
+                    } else if ($(elem).children('a').children('.CardHeadline').children('.signature').children('.byline').text() !== '') {
+                        return $(elem).children('a').children('.CardHeadline').children('.signature').children('.byline').text().split('By ')[1];
+                    } else {
+                        return 'Unknown'
+                    }
+                }
+                const date = $(elem).children('.CardHeadline').children('.signature').children('.Timestamp').attr('data-source');
+        
+                articles.push({
+                    title: title,
+                    summary: summary,
+                    link: link,
+                    author: author(),
+                    date: moment(date).format("dddd, MMMM Do YYYY, HH:mm"),
+                })
+            })
+            res.json(articles)
         })
-
-        // console.log(articles)
     })
 }
-)
