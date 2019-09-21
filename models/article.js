084fd3dedaac62300e3bfdcc78b09826a816eb1e
Magic-Mayo <mike.mayoii@gmail.com>
2019-09-20 18:11:28 -0700

    renamed

---
+++ models/article.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema ({
+    
+    title: String,
+    summary: String,
+    date: String,
+    comments: String
+})
+
+ArticleSchema.methods.addComment = function(post){
+    this.comments.push({post: post, date: Date.now()});
+}
+
+const Article = mongoose.model('ArticleSchema', ArticleSchema);
+module.exports = Article;
