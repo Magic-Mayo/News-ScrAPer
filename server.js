58e82fec9b5d53018f712a55e4d50b98663c76ce
Magic-Mayo <mike.mayoii@gmail.com>
2019-09-20 20:19:03 -0700

    added mongo connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
@@ -7,6 +8,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require('./routes/apiRoutes')(app);
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/News-ScrAPer";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
