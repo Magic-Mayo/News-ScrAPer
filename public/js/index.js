a34a2637dc3267f8daf69b65b4592f8f0013523a
Magic-Mayo <mike.mayoii@gmail.com>
2019-09-24 01:20:53 -0700

    tried setting up a loop to attach comments to articles, may need to revisit as it takes too long to do. finished click function for adding user comments to db and putting them on screen in the appropriate article

--- public/js/index.js
+++ public/js/index.js
@@ -1,84 +1,140 @@
 $(document).ready(()=>{
-    (() => {
+    const getArticle = () => {
         $.get('/articles', articles => {
-            // console.log(articles)
-            // let postCount = articles.reduce((a,b)=>{
-            //     return a.posts.length + b.posts.length;
-            // })
+            let commentCount = articles.comments.reduce((a,b)=>{
+                return a.comments.length + b.comments.length;
+            });
 
-            // $('.article-post-count').text(`${articles.length} articles | ${postCount} comments`)
-            
-            articles.map(article=>{
-                let postNum;
-                // !!articles.posts ? postNum = articles.posts.length : postNum = '';
-                $('.container').append(`
-                    <article class="media">
-                        <div class="media-content">
-                        <div class="content">
-                            <span>
-                                <b class="article-title">${article.title}</b>&emsp;<small>${article.date}</small>
-                                <br>
-                                <strong class="article-content">Author: </strong>${article.author}
-                                <p class="article-content"><a href="${article.link}" target="_blank">
-                                    ${article.summary}
-                                </a></p>
-                            </span>
-                        </div>
-                        <nav class="level">
-                                <button class="level-item post-btn">
-                                    <span class="icon">${postNum}&ensp;<i class="fas fa-comment-alt"></i></span>
-                                </button>
-                        </nav>
-                    </article>
-                    <article class="media hide add-comment">
-                    <div class="media-content">
-                        <div class="field">
-                            <p class="control">
-                                <textarea class="textarea" placeholder="Add a comment..."></textarea>
-                            </p>
-                        </div>
-                        <nav class="level">
-                            <div class="level-left">
-                                <div class="level-item">
-                                    <button class="button is-info" id="add-comment">Submit</button>
-                                </div>
-                            </div>
-                        </nav>
-                    </div>
-                </article>`
-                );
-
-                if(article.comments){
-                    $('.media-content').first().append(
-                        `<article class="media">
+            $('.article-post-count').text(`${articles.news.length} articles | ${commentCount} comments`);
+            console.log(articles)
+            articles.news.map(article=>{
+                
+                for (let i=0; i<articles.comments.length; i++){
+                    if (articles.comments[i].title === article.title){
+                        const comment = articles.comments[i].comments;
+                        let postNum = articles.comments[i].length;
+                        $('.container').append(`
+                        <article class="media">
                             <div class="media-content">
                             <div class="content">
-                                <p class="comment">${article.comment.post}</p>
+                                <span>
+                                    <b class="article-title">${article.title}</b>&emsp;<small>${article.date}</small>
+                                    <br>
+                                    <strong class="article-content">Author: </strong>${article.author}
+                                    <p class="article-content"><a href="${article.link}" target="_blank">
+                                        ${article.summary}
+                                    </a></p>
+                                </span>
                             </div>
+                            <nav class="level">
+                                    <button class="level-item post-btn">
+                                        <span class="icon post-num">${postNum}&ensp;<i class="fas fa-comment-alt"></i></span>
+                                    </button>
+                            </nav>
+                        </article>
+                        <article class="media hide add-comment">
+                        <div class="media-content">
+                            <div class="field">
+                                <p class="control">
+                                    <textarea class="textarea" placeholder="Add a comment..."></textarea>
+                                </p>
+                            </div>
+                            <nav class="level">
+                                <div class="level-left">
+                                    <div class="level-item">
+                                        <button class="button is-info" id="add-comment">Submit</button>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
                         </article>`
-                    )
+                        );
+                        
+                        comment.map(comment=>{
+                            $('.media-content').append(
+                                `<article class="media hide">
+                                    <div class="media-content">
+                                    <div class="content">
+                                        <p class="comment">${comment.post}</p>
+                                        <small>${moment(comment.date).format("dddd, MMMM Do YYYY, HH:mm")}</small>
+                                    </div>
+                                </article>`
+                            )
+                        })
+                    } else {
+                        postNum = '';
+                        $('.container').append(`
+                        <article class="media">
+                            <div class="media-content">
+                            <div class="content">
+                                <span>
+                                    <b class="article-title">${article.title}</b>&emsp;<small>${article.date}</small>
+                                    <br>
+                                    <strong class="article-content">Author: </strong>${article.author}
+                                    <p class="article-content"><a href="${article.link}" target="_blank">
+                                        ${article.summary}
+                                    </a></p>
+                                </span>
+                            </div>
+                            <nav class="level">
+                                    <button class="level-item post-btn">
+                                        <span class="icon post-num">${postNum}&ensp;<i class="fas fa-comment-alt"></i></span>
+                                    </button>
+                            </nav>
+                        </article>
+                        <article class="media hide add-comment">
+                        <div class="media-content">
+                            <div class="field">
+                                <p class="control">
+                                    <textarea class="textarea" placeholder="Add a comment..."></textarea>
+                                </p>
+                            </div>
+                            <nav class="level">
+                                <div class="level-left">
+                                    <div class="level-item">
+                                        <button class="button is-info" id="add-comment">Submit</button>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                        </article>`
+                        );
+                    }
                 }
             })
-        });
-    })();
+        })
+    };
+
+    getArticle();
 
     $(document).on('click', '.post-btn', function(e){
         e.preventDefault();
         $(this).parent().parent().parent().next().toggleClass('hide');
+        $(this).parent().siblings('.media').toggleClass('hide');
     })
 
     $(document).on('click', '#add-comment', function(e){
         e.preventDefault();
         const comment = $(this).parents('.media-content').children('.field').children().children().val().trim();
         const title = $(this).parents('.media-content').parent().prev().children().children().children().children('b').text();
-        $.post('articles/comments', {post: comment, title: title}).then(post=>{
+        $.post('/articles/comments', {post: comment, title: title}).then(post=>{
             console.log(post)
-            $('.media-content').first().append(
+            let comment = post.comments;
+            let date;
+            if (post.comments.length === 1){
+                comment = post.comments[0].post;
+                date = moment(post.comments[0].date).format("dddd, MMMM Do YYYY, HH:mm");
+            } else {
+                comment = post.comments.pop().post;
+                date = moment(post.comments.pop().date).format("dddd, MMMM Do YYYY, HH:mm");
+            }
+
+            $(this).parents('.media').prev().children().append(
                 `<article class="media">
                     <div class="media-content">
                     <div class="content">
-                        <p class="comment">${post.comment}</p>
-                        <small>${post.date}</small>
+                        <p class="comment">${comment}</p>
+                        <small>${date}</small>
                     </div>
                 </article>`
             );
